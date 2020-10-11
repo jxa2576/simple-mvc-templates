9ad0d612efe41bae3a5ea02a5e52c533d5a4f5d5
Jeb A <jxa2576@g.rit.edu>
2020-10-11 12:01:48 -0400

    Template tutorial

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const compression = require('compression');
 const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
 
 const router = require('./router.js');
 
@@ -11,12 +12,18 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const app = express();
 
-app.use('/assets', express.static(path.resolve(`${__dirname}/../client/`)));
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: '',
+}));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
 
 app.use(cookieParser());
 
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,27 +3,43 @@
 let name = 'unknown';
 
 const hostIndex = (req, res) => {
-
+  res.render('index', {
+    title: 'Home',
+    pageName: 'Home Page',
+    userName: name,
+  });
 };
 
 const hostPage1 = (req, res) => {
-
+  res.render('page1', {
+    title: 'Page 1',
+  });
 };
 
 const hostPage2 = (req, res) => {
-
+  res.render('page2');
 };
 
 const getName = (req, res) => {
-
+  res.json({name});
 };
 
 const setName = (req, res) => {
+  if(!req.body.firstname || !req.body.lastname){
+    return res.status(400).json({
+      error: 'firstname and lastname are both required!',
+      id: 'badRequest',
+    });
+  }
 
+  name = `${req.body.firstname} ${req.body.lastname}`;
+  res.json({name});
 };
 
 const notFound = (req, res) => {
-
+  res.status(404).render('notFound', {
+    page: req.url,
+  });
 };
 
 module.exports = {
